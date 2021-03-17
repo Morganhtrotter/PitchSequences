666186f13d726097cf809116c330fc4db53ec20e
Morgan Trotter <mhtrotte@ucsc.edu>
2021-03-16 19:28:45 -0700

    Added buttons and string logic for adding own data

---
+++ addData.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+	var sequence = "";
+
+	$("fastballButton").onclick = function() {
+		sequence += "fastball-";
+		$("sequenceText").innerHTML = sequence;
+	}
+
+	$("changeupButton").onclick = function() {
+		sequence += "changeup-";
+		$("sequenceText").innerHTML = sequence;
+	}
+
+	$("curveballButton").onclick = function() {
+		sequence += "curveball-";
+		$("sequenceText").innerHTML = sequence;
+	}
+
+	$("sliderButton").onclick = function() {
+		sequence += "slider-";
+		$("sequenceText").innerHTML = sequence;
+	}
+
+	$("strikeoutButton").onclick = function() {
+		sequence += "strikeout";
+		$("sequenceText").innerHTML = sequence;
+	}
+
+	$("groundoutButton").onclick = function() {
+		sequence += "groundout";
+		$("sequenceText").innerHTML = sequence;
+	}
+
+	$("reset").onclick = function() {
+		sequence = "";
+		$("sequenceText").innerHTML = sequence;
+	}
+}
+
+function $(id) {
+	return document.getElementById(id);
+}
