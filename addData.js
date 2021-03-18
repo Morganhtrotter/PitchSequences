86311aa09bc3eb625078afc4dbe35c955a91aa33
Morgan Trotter <mhtrotte@ucsc.edu>
2021-03-18 12:44:56 -0700

    Removed add AB buttons and changed data

--- addData.js
+++ addData.js
@@ -1,49 +1,4 @@
 window.onload = function() {
-	var sequence = "";
-
-	$("fastballButton").onclick = function() {
-		sequence += "fastball-";
-		$("sequenceText").innerHTML = sequence;
-	}
-
-	$("changeupButton").onclick = function() {
-		sequence += "changeup-";
-		$("sequenceText").innerHTML = sequence;
-	}
-
-	$("curveballButton").onclick = function() {
-		sequence += "curveball-";
-		$("sequenceText").innerHTML = sequence;
-	}
-
-	$("sliderButton").onclick = function() {
-		sequence += "slider-";
-		$("sequenceText").innerHTML = sequence;
-	}
-
-	$("strikeoutButton").onclick = function() {
-		sequence += "strikeout";
-		$("sequenceText").innerHTML = sequence;
-	}
-
-	$("groundoutButton").onclick = function() {
-		sequence += "groundout";
-		$("sequenceText").innerHTML = sequence;
-	}
-
-	$("reset").onclick = function() {
-		sequence = "";
-		$("sequenceText").innerHTML = sequence;
-	}
-
-	$("add").onclick = function() {
-		console.log("here");
-		WriteToFile(sequence);
-	}
-}
-
-function WriteToFile(sequence) {
-	// Look into localStorage
 }
 
 function $(id) {
